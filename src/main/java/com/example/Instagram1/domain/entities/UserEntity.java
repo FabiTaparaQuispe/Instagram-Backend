bc9fb1ea37d891672775547e2450cdae70dd377e
FabiTaparaQuispe <ftapara@unsa.edu.pe>
2024-01-11 12:14:48 -0500

    Sprig Boot initial Project with Maven, application properties and data source user Entity and table

--- src/main/java/com/example/Instagram1/domain/entities/UserEntity.java
+++ src/main/java/com/example/Instagram1/domain/entities/UserEntity.java
@@ -1,10 +1,22 @@
 package com.example.Instagram1.domain.entities;
 
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Table(name="users")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity(name="user")
+
 
 public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String name;
     private String lastname;
---
+++ src/main/java/com/example/Instagram1/domain/repositories/UserRepository.java
@@ -0,0 +1,10 @@
+package com.example.Instagram1.repositories;
+
+import com.example.Instagram1.domain.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository <UserEntity, Long>{
+
+}
+
+
